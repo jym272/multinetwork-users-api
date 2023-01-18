cc7a1208e7d8758deabaf32eb2538623c5d43a8c
Jorge Clavijo <jym272@gmail.com>
2023-01-18 19:50:33 -0300

    feat: postgres container in tests

--- cmds.sh
+++ cmds.sh
@@ -6,3 +6,8 @@ curl -s -X POST -H "Content-Type: application/json" -d '{"email": "jym272@email.
 
 # login
 curl -s -X POST -H "Content-Type: application/json" -d '{"email": "jym272@email.com", "password": "Adiniii27A"}' http://localhost:3050/login | jq
+#result:
+#{
+#  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6eyJhdXRoZW50aWNhdGUiOnRydWV9LCJpYXQiOjE2NzQwNzgyNjcsImV4cCI6MTY3NDE2NDY2NywiYXVkIjoidGFza3MtYXBpIiwiaXNzIjoiYXV0aC1hcGkiLCJzdWIiOiJqeW0yNzJAZW1haWwuY29tIiwianRpIjoiMSJ9.H4C6-MeNE_zq5U_Rah_hqq07eeT2iX89sK05XuSn9Pw"
+#}
+token=$(curl -s -X POST -H "Content-Type: application/json" -d '{"email": "jym272@email.com", "password": "Adiniii27A"}' http://localhost:3050/login | jq -r '.token')
